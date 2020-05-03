f929f072b9d9f6515c470a12142d6af348ff96a7
sophiaju <60048531+sophiaju@users.noreply.github.com>
2020-05-02 22:19:19 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.homebody;
 
-import android.Manifest;
-import android.location.Location;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,16 +7,10 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import io.radar.sdk.Radar;
-import io.radar.sdk.RadarTrackingOptions;
-import io.radar.sdk.model.RadarEvent;
-import io.radar.sdk.model.RadarUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,23 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
-
-        //radar initialization
-        Radar.initialize(this, "prj_test_pk_2ff6a35629ab2b24d0d97e55ba35142982a45a0e");
-        ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION }, 123);
-        //start background tracking
-        Radar.startTracking(RadarTrackingOptions.RESPONSIVE);
+        });*/
     }
 
     @Override
